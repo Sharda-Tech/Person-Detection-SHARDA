f51f16d9fc12a8234fe1f2448726de919ad749ca
jarvis2324 <gauravbarua614@gmail.com>
2022-05-08 19:30:50 +0530

    Added 2nd API

--- yolov5/person_detection_day.py
+++ yolov5/person_detection_day.py
@@ -2,10 +2,70 @@ import cv2
 import numpy as np
 from elements.yolo import OBJ_DETECTION
 from email_sender import send_email
+import requests
+import time
 
 
+
+
+def request_status():
+  url = "http://as99.zvastica.solutions/appapi/checkdevicestatus"
+
+  payload = "{\n    \"device_id\":\"2\"\n }"
+  headers = {
+    'Content-Type': 'application/json'
+  }
+
+  response = requests.request("POST", url, headers=headers, data = payload)
+
+  #print(response.text.encode('utf8'))
+
+  #convert byte to string
+
+  print(response.text)
+
+  #split string using ','
+
+  str = response.text.split(',')
+
+  print(str[1])
+
+  status = str[1].split(':')
+
+  print(status[1])
+
+  return 'true'
+
+
+def sent_video():
+    url = "http://as99.zvastica.solutions/appapi/submitviolence"
+
+    payload = {'device_id': '1234'}
+    files = [
+    ('file', open('./output.avi','rb'))
+    ]
+    headers = {
+    'Cookie': 'ci_session=sn7n11lsss9vdlrej79sq6s1o0c5mm3r'
+    }
+
+    response = requests.request("POST", url, headers=headers, data = payload, files = files)
+
+    print(response.text.encode('utf8'))
+
+    return True
 
 def predict():
+
+    prev_time = 0
+    new_time = time.time()
+
+    video_sent_status = False
+    number_of_person_detection = 0
+    frames_counter = 0
+    frames = []
+    not_detected_frames_thresh = 10
+    number_of_frames_not_detected = 0
+
     Object_classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
                     'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
                     'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
@@ -23,12 +83,38 @@ def predict():
     #print(gstreamer_pipeline(flip_method=0))
     #cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     cap = cv2.VideoCapture('./person.mp4')
+
+    if(prev_time == 0):
+
+        status_of_device = request_status()
+        #status_of_device = 'false'
+
+        prev_time = new_time
+        new_time = time.time()
+
+
     if cap.isOpened():
+
+
+        
         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
         # Window
-        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0 :
             ret, frame = cap.read()
-            if ret:        
+            new_time = time.time()
+            #print("Previous Time", prev_time)
+            #print("New Time", new_time)
+
+            if((new_time - prev_time) >= 10):
+
+                status_of_device = request_status()
+                print(status_of_device)
+                #status_of_device = 'true'
+
+                prev_time = new_time
+                new_time = time.time()
+
+            if ret and status_of_device == 'true':
                 # detection process
                 objs = Object_detector.detect(frame)
                 dets = []
@@ -37,7 +123,8 @@ def predict():
                     # print(obj)
                     label = obj['label']
                     if((label == 'person')):
-                        print(label)
+                        number_of_frames_not_detected = 0
+                        #print(label)
                         score = obj['score']
                         [(xmin,ymin),(xmax,ymax)] = obj['bbox']
                         #print(xmin,ymin,xmax,ymax)
@@ -46,18 +133,35 @@ def predict():
                         #color = Object_colors[Object_classes.index(label)]
                         frame = cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (255,0,0), 2) 
                         frame = cv2.putText(frame, f'{label} ({str(score)})', (xmin,ymin), cv2.FONT_HERSHEY_SIMPLEX , 0.75, (0,255,255), 1, cv2.LINE_AA)
+                        print(frames_counter)
+                        if(video_sent_status == False):
+                            if(frames_counter < 600):
+                                frames_counter = frames_counter + 1
+                                frames.append(frame)
+                            elif(frames_counter >= 600):
+                                #write a list of frames in a video
+                                out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 60, (frame.shape[1],frame.shape[0]))
+                                for i in range(len(frames)):
+                                    out.write(frames[i])
+                                out.release()
+                                video_sent_status = sent_video()
+                                if video_sent_status == True:
+                                    print("Video sent")
+                                frames = []
+
+                    elif(number_of_frames_not_detected < not_detected_frames_thresh):
+                        number_of_frames_not_detected = number_of_frames_not_detected + 1
+                    elif(number_of_frames_not_detected >= not_detected_frames_thresh):
+                        video_sent_status = False
+                        frames_counter = 0
+                        frames = []
+
             cv2.imshow("CSI Camera", frame)
             keyCode = cv2.waitKey(30)
             if keyCode == ord('q'):
                 break      
         cap.release()
         cv2.destroyAllWindows()
-
-    #write a list of frames in a video
-    #out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
-    #for i in range(len(frames)):
-    #    out.write(frames[i])
-    #out.release()
 
     else:
         print("Unable to open camera")
