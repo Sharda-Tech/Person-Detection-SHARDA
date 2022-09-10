be16ba873923f1370384471d76500de2d48ae2f2
jarvis2324 <gauravbarua614@gmail.com>
2022-09-10 18:15:06 +0530

    Restart SC fixed

--- yolov5/sc_updated.py
+++ yolov5/sc_updated.py
@@ -4,56 +4,71 @@ import requests
 sio = socketio.Client()
 
 
+class connect():
+    def __init__(self):
+        self.serial = "1112233"
+        try:
+            self.device_id = self.register()
+        except:
+            print("Do Not Know the ID please connect to the Internet")
+            with open('./device_id.txt', 'r') as f:
+                self.device_id = f.read()
+                self.device_id = self.device_id[1:-1]
+                f.close()
+        self.id = self.serial
+        self.name = self.device_id
 
 
 
-def register(serial):
-    myserial =  serial
-    #myserial = "0013"
-    url = "http://as99.zvastica.solutions/appapi/adddevicebyhardware"
+    def register(self):
+        myserial =  self.serial
+        #myserial = "0013"
+        url = "http://as99.zvastica.solutions/appapi/adddevicebyhardware"
 
-    #payload="{\n   \n \"hardwar_id\" :\"devicde_serial_no\"\n      \n        \n}"
-    payload = "{ \n \n \"hardwar_id\" : \"" + myserial + "\" \n \n \n}"
-    print(payload)
-    headers = {
-    'Content-Type': 'application/json',
-    'Cookie': 'ci_session=jsunnmfv9mlfgs7cbcu0nlt8rg2op8up'
-    }
+        #payload="{\n   \n \"hardwar_id\" :\"devicde_serial_no\"\n      \n        \n}"
+        payload = "{ \n \n \"hardwar_id\" : \"" + myserial + "\" \n \n \n}"
+        print(payload)
+        headers = {
+        'Content-Type': 'application/json',
+        'Cookie': 'ci_session=jsunnmfv9mlfgs7cbcu0nlt8rg2op8up'
+        }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+        response = requests.request("POST", url, headers=headers, data=payload)
 
-    print(response.text)
+        print(response.text)
 
-    #split string using ','
-    str = response.text.split(',')
-    for i in str:
-        #print(i)
-        k = i.split(':')   
-        if(k[0] == '{"device_id"'):
-            #print("Device is not registered")
-            print("Device id is", k[1][1:-1])
-            #save the device id in a text file
-            device_id = k[1][1:-1]
-            return device_id
-
-
-@sio.on('seq-num')
-def func(msg):
-    print("Message is",msg)
-    print("Writting")
-    with open('./device_status.txt', 'w') as f:
-        f.writelines(msg)
-    f.close()
+        #split string using ','
+        str = response.text.split(',')
+        for i in str:
+            #print(i)
+            k = i.split(':')   
+            if(k[0] == '{"device_id"'):
+                #print("Device is not registered")
+                print("Device id is", k[1][1:-1])
+                #save the device id in a text file
+                device_id = k[1][1:-1]
+                return device_id
 
 
-@sio.on('connect')
-def reg_client(id, name):
-    print("Id is ", id, "Name is", name)
-    sio.emit('storeClientInfo', {'customId': id, 'name': name})
-    print('Done')
+    @sio.on('seq-num')
+    def func(msg):
+        print("Message is",msg)
+        print("Writting")
+        with open('./device_status.txt', 'w') as f:
+            f.writelines(msg)
+        f.close()
+
+
+    #@sio.on('connect')
+    def reg_client(self):
+        print("Id is ", self.id, "Name is", self.name)
+        sio.emit('storeClientInfo', {'customId': self.id, 'name': self.name})
+        print('Done')
 
 
 if __name__ == "__main__":
+
+    
 
     while (True):
         try:
@@ -63,16 +78,6 @@ if __name__ == "__main__":
             print("Retrying")
             continue
     #serial = getserial()
-    serial = "1112233"
-    try:
-        device_id = register(serial)
-    except:
-        print("Do Not Know the ID please connect to the Internet")
-        with open('/home/pi/Person-Detection/yolov5/device_id.txt', 'r') as f:
-            device_id = f.read()
-            device_id = device_id[1:-1]
-            f.close()
-    id = serial
-    name = device_id
-    reg_client(id, name)
+    cc = connect()
+    cc.reg_client()
 
